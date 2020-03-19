e670a3483f06d465d5034b944a65fb4b7b2af179
Manuel Meraz <meraz.manuel@gmail.com>
2020-03-18 20:33:21 -0400

    change code to light up led

--- main.cpp
+++ main.cpp
@@ -1,8 +1,18 @@
-#include <iostream>
 #include <wiringPi.h>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    wiringPiFailure(4, "hello");
+int main()
+{
+    wiringPiSetup();
+    pinMode(0, OUTPUT);
+
+    for (;;)
+    {
+        digitalWrite(0, HIGH);
+        delay(1000);
+        digitalWrite(0, LOW);
+        delay(1000);
+    }
     return 0;
 }
+
+
