c839b82e5a323f0ce7287c1b60f3dc6d961b0687
Manuel Meraz <meraz.manuel@gmail.com>
2020-03-30 18:42:49 -0400

    Add in more cmakelists to properly configure project. Add in skeleton motor class.

---
+++ src/gpio_bridge/src/motor_controls/Motor.cpp
@@ -0,0 +1,5 @@
+//
+// Created by manny on 3/30/20.
+//
+
+#include "Motor.hpp"
---
+++ src/gpio_bridge/src/motor_controls/Motor.hpp
@@ -0,0 +1,15 @@
+//
+// Created by manny on 3/30/20.
+//
+
+#ifndef GPIO_BRIDGE_MOTOR_HPP
+#define GPIO_BRIDGE_MOTOR_HPP
+
+namespace motor_controls {
+
+class Motor
+{
+};
+
+} // namespace motor_controls
+#endif // GPIO_BRIDGE_MOTOR_HPP
