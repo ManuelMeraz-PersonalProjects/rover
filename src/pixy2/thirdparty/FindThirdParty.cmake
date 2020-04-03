53e9572fe3554baa0099d38ac42ac7094cb14387
Manuel Meraz <meraz.manuel@gmail.com>
2020-04-03 19:00:58 -0400

    lib pixy2 actually installs, let's see what happens next

--- src/pixy2/thirdparty/FindThirdParty.cmake
+++ src/pixy2/thirdparty/FindThirdParty.cmake
@@ -1,7 +1,3 @@
 set(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} ${CMAKE_CURRENT_SOURCE_DIR}/thirdparty/modules)
-
-find_package(wiringPi REQUIRED)
-find_package(Threads REQUIRED)
-find_library(rt rt REQUIRED)
-find_library(crypt crypt REQUIRED)
+find_package(libpixyusb2 REQUIRED)
 
