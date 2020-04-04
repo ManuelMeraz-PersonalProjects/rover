f49430b92a01147194f43ceade8374ac89f71f55
Manuel Meraz <meraz.manuel@gmail.com>
2020-04-04 10:32:48 -0400

    can build chirp demo

--- src/pixy2/thirdparty/FindThirdParty.cmake
+++ src/pixy2/thirdparty/FindThirdParty.cmake
@@ -1,10 +1,13 @@
 set(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} ${CMAKE_CURRENT_SOURCE_DIR}/thirdparty/modules)
 
-find_package(Pixy2 REQUIRED)
-# LIBUSB_1_INCLUDE_DIRS LIBUSB_1_LIBRARIES
 find_package(LibUSB REQUIRED)
+add_Library(usb1 STATIC IMPORTED)
+set_property(TARGET usb1 PROPERTY IMPORTED_LOCATION ${LIBUSB_1_LIBRARIES})
+target_include_directories(usb1 INTERFACE ${LIBUSB_1_INCLUDE_DIRS})
 
+find_package(Pixy2 REQUIRED)
 add_library(pixy2 STATIC IMPORTED)
-target_link_libraries(pixy2 INTERFACE ${LIBUSB_1_LIBRARIES} ${LIBPIXY2_LIBRARIES})
-target_include_directories(pixy2 INTERFACE ${LIBUSB_1_INCLUDE_DIRS} ${LIBPIXY2_INCLUDE_DIRS})
+set_property(TARGET pixy2 PROPERTY IMPORTED_LOCATION ${LIBPIXY2_LIBRARIES})
+target_include_directories(pixy2 INTERFACE ${LIBPIXY2_INCLUDE_DIRS})
+target_link_libraries(pixy2 INTERFACE usb1)
 
