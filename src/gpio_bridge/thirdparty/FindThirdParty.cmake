9c85f57ea9eb160d69cb7b2b606d96a29584398f
Manuel Meraz <meraz.manuel@gmail.com>
2020-04-04 18:32:05 -0400

    can link with gpio

--- src/gpio_bridge/thirdparty/FindThirdParty.cmake
+++ src/gpio_bridge/thirdparty/FindThirdParty.cmake
@@ -1,7 +1,6 @@
-set(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} ${CMAKE_CURRENT_SOURCE_DIR}/thirdparty/modules)
+set(CMAKE_MODULE_PATH ${CMAKE_CURRENT_SOURCE_DIR}/thirdparty/modules)
 
-find_package(wiringPi REQUIRED)
-find_package(Threads REQUIRED)
-find_library(rt rt REQUIRED)
-find_library(crypt crypt REQUIRED)
-
+find_package(gpio REQUIRED)
+add_library(odroid::gpio STATIC IMPORTED)
+set_property(TARGET odroid::gpio PROPERTY IMPORTED_LOCATION ${GPIO_LIBRARIES})
+target_include_directories(odroid::gpio INTERFACE ${GPIO_INCLUDE_DIRS})
