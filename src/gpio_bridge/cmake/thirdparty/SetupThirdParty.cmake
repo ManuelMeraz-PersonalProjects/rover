3091b95106a39a9c95c36a54d59da565b3994fcb
Manuel Meraz <meraz.manuel@gmail.com>
2020-04-07 21:22:01 -0400

    turn gpio_bridge into ros2 package

--- src/gpio_bridge/cmake/thirdparty/SetupThirdParty.cmake
+++ src/gpio_bridge/cmake/thirdparty/SetupThirdParty.cmake
@@ -5,3 +5,11 @@ find_package(OdroidGPIO REQUIRED)
 add_library(libodroidgpio INTERFACE)
 target_link_libraries(libodroidgpio INTERFACE ${ODROIDGPIO_LIBRARIES})
 target_include_directories(libodroidgpio INTERFACE ${ODROIDGPIO_INCLUDE_DIRS})
+
+##########################
+# ROS 2
+##########################
+find_package(ament_cmake REQUIRED)
+# uncomment the following section in order to fill in
+# further dependencies manually.
+# find_package(<dependency> REQUIRED)
