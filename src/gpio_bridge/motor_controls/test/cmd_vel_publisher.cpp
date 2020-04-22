7a695dc2a78302432a2924ceeb9661edb79e1117
Manuel Meraz <meraz.manuel@gmail.com>
2020-04-22 19:40:10 -0400

    test cmd vel publisher completed

---
+++ src/gpio_bridge/motor_controls/test/cmd_vel_publisher.cpp
@@ -0,0 +1,65 @@
+#include <geometry_msgs/msg/twist_stamped.hpp>
+#include <rclcpp/rclcpp.hpp>
+
+constexpr auto QUALITY_OF_SERVICE = 10;
+using namespace std::chrono_literals;
+
+/* This example creates a subclass of Node and uses std::bind() to register a
+ * member function as a callback from the timer. */
+
+class IMUBNO055Publisher : public rclcpp::Node
+{
+ public:
+   IMUBNO055Publisher() : Node("imu_bno055_publisher")
+   {
+      m_command_velocity_publisher =
+         this->create_publisher<geometry_msgs::msg::TwistStamped>("/motor_controller/cmd_vel", QUALITY_OF_SERVICE);
+
+      m_timer = this->create_wall_timer(400ms, [this] {
+         geometry_msgs::msg::TwistStamped message;
+         message.header.stamp = get_clock()->now();
+         message.header.frame_id = "base_link";
+         message.twist.linear.x = m_linear_command;
+         message.twist.linear.y = 0;
+         message.twist.linear.z = 0;
+
+         message.twist.angular.x = 0;
+         message.twist.angular.y = 0;
+         message.twist.angular.z = m_angular_command;
+
+         if (m_forward) {
+            m_linear_command += DELTA;
+            m_angular_command += DELTA;
+         } else {
+            m_linear_command -= DELTA;
+            m_angular_command -= DELTA;
+         }
+
+         if (std::abs(m_linear_command) > 1) {
+            m_forward = !m_forward;
+         }
+
+         const auto& logger = get_logger();
+         RCLCPP_INFO(logger, "Publishing out a linear command of: %f", m_linear_command);
+         RCLCPP_INFO(logger, "Publishing out a angular command of: %f", m_angular_command);
+         m_command_velocity_publisher->publish(message);
+      });
+   }
+
+ private:
+   static constexpr double DELTA = 0.05;
+
+   bool m_forward{true};
+   double m_linear_command{0.0};
+   double m_angular_command{0.0};
+   rclcpp::TimerBase::SharedPtr m_timer{};
+   rclcpp::Publisher<geometry_msgs::msg::TwistStamped>::SharedPtr m_command_velocity_publisher{};
+};
+
+int main(int argc, char* argv[])
+{
+   rclcpp::init(argc, argv);
+   rclcpp::spin(std::make_shared<IMUBNO055Publisher>());
+   rclcpp::shutdown();
+   return 0;
+}
