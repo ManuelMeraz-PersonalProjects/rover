c7fce3e108fa1c55c28712bc1daa0887b5b64c23
Manuel Meraz <meraz.manuel@gmail.com>
2020-04-24 19:52:44 -0400

    rosify test motor controls

--- src/gpio_bridge/motor_controls/test/test_motor_controls.cpp
+++ src/gpio_bridge/motor_controls/test/test_motor_controls.cpp
@@ -2,22 +2,22 @@
 
 #include <chrono>
 #include <iostream>
+#include <rclcpp/rclcpp.hpp>
 
 using namespace std::chrono_literals;
 
 auto main() -> int
 {
+   const auto& logger = rclcpp::get_logger("test_motor_controls");
    auto& controller = motor_controls::MotorController::get();
 
    constexpr uint8_t DUTY_CYCLE = 100;
 
-   std::cout << "Actuating motors forward for 10 seconds at 100% duty cycle" << std::endl;
+   RCLCPP_INFO(logger, "Actuating motors forward for 10 seconds at 100%% duty cycle");
    motor_controls::Command command{motor_controls::Direction::FORWARD, DUTY_CYCLE, 10s};
    controller.actuate(command);
 
-   std::cout << "Actuating motors reverse for 10 seconds at 100% duty cycle" << std::endl;
+   RCLCPP_INFO(logger, "Actuating motors reverse for 10 seconds at 100% duty cycle");
    command.direction = motor_controls::Direction::REVERSE;
    controller.actuate(command);
-
-   controller.stop();
 }
