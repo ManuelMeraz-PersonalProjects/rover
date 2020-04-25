798c54a1142942672f2ff2f48eee9bdff36136aa
Manuel Meraz <meraz.manuel@gmail.com>
2020-04-25 11:41:34 -0400

    installing motor controls node properly

--- src/rover_base/launch/rover.launch.py
+++ src/rover_base/launch/rover.launch.py
@@ -10,15 +10,31 @@ prefixes = [
 
 prefixes = [prefix + '=' + environ[prefix] for prefix in prefixes]
 
+node_prefix = 'sudo ' + ' '.join(prefixes)
+
 
 def generate_launch_description():
     return LaunchDescription([
         Node(
-            prefix='sudo ' + ' '.join(prefixes),
+            prefix=node_prefix,
             name='imu_bno055_publisher',
             package='imu_adafruit_bno055',
             node_executable='imu_bno055_publisher',
             output='screen',
+        ),
+        Node(
+            prefix=node_prefix,
+            name='diff_drive_controller_node',
+            package='motor_controls',
+            node_executable='diff_drive_controller_node',
+            output='screen',
+        #     parameters=[{
+        #         'serial_port': '/dev/ttyUSB0',
+        #         'serial_baudrate': 115200,
+        #         'frame_id': 'laser',
+        #         'inverted': False,
+        #         'angle_compensate': True,
+        #     }],
         ),
         # Node(
         #     node_name='rplidarNodeClient',
